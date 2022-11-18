76db4ba04c9baa78ba5abb2fcca0a1473e4f7cd7
denn-acrymoore <denn.sebastian.e@gmail.com>
2022-11-18 10:06:48 +0700

    Buat dan Install Ulang Utapis Package
    Changes:
    - Build ulang module proyek utapis_sintaksis_flask_server ini.
    - Install ulang module utapis_sintaksis_flask_server ini.
    - Perbaiki typo di setup.py ("Flask" menjadi "flask").

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="utapis-sintaksis-flask-server",
     version="1.0.0",
-    install_requires=["Flask", "anyascii", "nltk", "python-crfsuite", "waitress"],
+    install_requires=["flask", "anyascii", "nltk", "python-crfsuite", "waitress"],
     include_package_data=True,
     packages=find_packages(where="utapis_sintaksis_flask_server"),
     package_dir={"": "utapis_sintaksis_flask_server"},
